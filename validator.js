6877fc9964cb6e73c886302f0055dacda405e815
Tram Nguyen <tramngoc1402@gmail.com>
2022-07-11 16:25:41 +0300

    Add Rules and Implement Submit Form

--- validator.js
+++ validator.js
@@ -1,11 +1,21 @@
 // Đối tượng
 function Validator(options) {
+  var selectorRules = {};
   //Hàm thực hiện validate
   function validate(inputElement, rule) {
-    var errorMessage = rule.test(inputElement.value);
+    var errorMessage;
     var errorElement = inputElement.parentElement.querySelector(
       options.errorSelector
     );
+
+    //Lấy ra các rules của selector
+    var rules = selectorRules[rule.selector];
+    //Lặp qua từng rule và kiểm tra
+    //Nếu có lỗi thì dừng việc kiểm tra
+    for (var i = 0; i < rules.length; ++i) {
+      errorMessage = rules[i](inputElement.value);
+      if (errorMessage) break;
+    }
 
     if (errorMessage) {
       errorElement.innerHTML = errorMessage;
@@ -14,12 +24,59 @@ function Validator(options) {
       errorElement.innerHTML = '';
       inputElement.parentElement.classList.remove('invalid');
     }
+
+    return !errorMessage;
   }
   //Lấy element của form cần validate
   var formElement = document.querySelector(options.form);
-  console.log(options.rules);
+
   if (formElement) {
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      var isFormValid = true;
+
+      //Thực hiện lặp qua từng rule và validate
+      options.rules.forEach(function (rule) {
+        var inputElement = formElement.querySelector(rule.selector);
+        var isValid = validate(inputElement, rule);
+        if (!isValid) {
+          isFormValid = false;
+        }
+      });
+
+      if (isFormValid) {
+        //Trường hợp submit với JavaScript
+        if (typeof options.onSubmit === 'function') {
+          var enableInputs = formElement.querySelectorAll(
+            '[name]:not([disabled])'
+          );
+
+          var formValues = Array.from(enableInputs).reduce(function (
+            values,
+            input
+          ) {
+            return (values[input.name] = input.value) && values;
+          },
+          {});
+          options.onSubmit(formValues);
+        }
+        //Trường hợp submit với hành vi mặc định
+        else {
+          formElement.submit();
+        }
+      }
+    };
+
+    //Xử lý lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input)
     options.rules.forEach(function (rule) {
+      //Lưu lại các rules cho mỗi input
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
       var inputElement = formElement.querySelector(rule.selector);
 
       if (inputElement) {
@@ -44,22 +101,22 @@ function Validator(options) {
 //Nguyên tắc của các rules
 //1. Khi có lỗi => Trả về message lỗi
 //2. Khi hợp lệ => Không trả ra cái gì cả (undefined)
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
-      return value.trim() ? undefined : 'Please fill in the blank!';
+      return value.trim() ? undefined : message || 'Please fill in the blank!';
     },
   };
 };
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
       var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
       return regex.test(value)
         ? undefined
-        : 'Please fill a valid email address!';
+        : message || 'Please fill a valid email address!';
     },
   };
 };
@@ -70,6 +127,16 @@ Validator.minLength = function (selector, min) {
       return value.length >= min
         ? undefined
         : `Please fill at least ${min} characters`;
+    },
+  };
+};
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || 'Incorrect value';
     },
   };
 };
